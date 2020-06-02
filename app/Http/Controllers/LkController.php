600e93c7ac729256b95cff6a2101d397cb3dbf6b
/usr/bin/vi <support@beget.ru>
2020-06-02 16:58:15 +0300

    Перенос скрипта вне цикла

--- app/Http/Controllers/LkController.php
+++ app/Http/Controllers/LkController.php
@@ -19,9 +19,7 @@ class LkController extends Controller
 
     public function index(Request $request)
     {
-        //dump($request->form);
         if ($request->has('form')) {
-
             foreach ($request->form as $form => $value) {
                 //dump($value);
                 $message = Result::find($value['id']);
@@ -29,13 +27,22 @@ class LkController extends Controller
                 $message->save();
                 Session::flash('message', 'Комментарий обновлён');
             }
-
-
         }
+        if ($request->has('boot'))
+        {
+            $message = new Result;
+            $message->message = $request->addmessage;
+            $message->date = '2020-06-05';
+            $user = User::find(Auth::id());
+            $user->results()->save($message);
+            Session::flash('message', 'Добавлена новая запись');
+        }
+        //dump(DB::table('result'));
         $messages = User::find(Auth::id())->results()->get();
-        //dump(DB::table('results')->select(DB::raw('date'))->groupBy('date')->get());
+        $results=DB::table('results')->select(DB::raw('date'))->groupBy('date')->get();
         return view('lk.show', [
-            'messages' => $messages
+            'messages' => $messages,
+            'results'=>$results
         ]);
     }
 }
--- resources/views/lk/show.blade.php
+++ resources/views/lk/show.blade.php
@@ -6,13 +6,59 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Личный кабинет пользователя {{Auth::user()->name}}</div>
+                    @if (Session::has('message'))
+                        <div class="alert alert-success" id="error-message">{{Session::get('message')}}</div>
+                        <script>
+                            setTimeout(function () {
+                                $('#error-message').hide();
+                            }, 1500)
+                        </script>
+                    @endif
+                    <div class="panel-heading">
+                        <p><a href="#myModal1" class="btn btn-primary" data-toggle="modal">Добавить заметку</a></p>
+                        <div id="myModal1" class="modal fade">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×
+                                        </button>
+                                        <h4 class="modal-title">Запишите заметку, чтобы не забыть</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form method="post">
+                                            {{csrf_field()}}
+
+                                            <label for="inputState">Выбрать дату итогов:</label> <select
+                                                    class="form-control">
+                                                @foreach($results as $result)
+                                                    <option>{{$result->date}}</option>
+                                                @endforeach
+                                            </select>
+                                            <textarea class="form-control" id="exampleFormControlTextarea1"
+                                                      rows="3" name="addmessage"
+                                                      style="max-width: 100%"
+                                                      placeholder="Введите заметку"></textarea><br>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">
+                                                    Закрыть
+                                                </button>
+                                                <input type="submit" class="btn btn-primary"
+                                                       name="boot">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
 
                     @foreach($messages as $message)
 
                         <div class="panel-body">
                             <b>Дата итогов: {{$message->date}}</b><br>
                             <b>ID сообщения: {{$message->id}}</b><br>
-                            <p onclick="openbox{{$message->id}}('box{{$message->id}}')" style="display: block"
+                            <p onclick="openbox('{{$message->id}}')" style="display: block"
                                id="id-message{{$message->id}}">{!! nl2br($message->message) !!}</p>
 
                             <form id="box{{$message->id}}" style="display: none;" method="post">
@@ -23,29 +69,31 @@
                                 <input type="hidden" name="form[{{$message->id}}][id]" value="{{$message->id}}">
                                 <input type="submit" name="form[{{$message->id}}][edit-message]" class="btn btn-primary"
                                        value="Сохранить">
-                                <button id="end" onclick="end{{$message->id}}()" type="button"
+                                <button id="end{{$message->id}}" onclick="end({{$message->id}})" type="button"
                                         class="btn btn-outline-light pull-right">Отмена
                                 </button>
                             </form>
-                            <script>
-                                function openbox{{$message->id}}(id) {
-                                    display = document.getElementById(id).style.display;
-                                    if (display == 'none') {
-                                        document.getElementById(id).style.display = 'block';
-                                        document.getElementById('id-message{{$message->id}}').style.display = 'none';
-                                    } else {
-                                        document.getElementById(id).style.display = 'none';
-                                        document.getElementById('id-message{{$message->id}}').style.display = 'block';
-                                    }
-                                }
 
-                                function end{{$message->id}}() {
-                                    document.getElementById('box{{$message->id}}').style.display = 'none';
-                                    document.getElementById('id-message{{$message->id}}').style.display = 'block';
-                                }
-                            </script>
                         </div>
                     @endforeach
+
+                    <script>
+                        function openbox(id) {
+                            display = document.getElementById('box' + id).style.display;
+                            if (display == 'none') {
+                                document.getElementById('box' + id).style.display = 'block';
+                                document.getElementById('id-message' + id).style.display = 'none';
+                            } else {
+                                document.getElementById('box' + id).style.display = 'none';
+                                document.getElementById('id-message' + id).style.display = 'block';
+                            }
+                        }
+
+                        function end(id) {
+                            document.getElementById('box' + id).style.display = 'none';
+                            document.getElementById('id-message' + id).style.display = 'block';
+                        }
+                    </script>
 
                 </div>
             </div>
